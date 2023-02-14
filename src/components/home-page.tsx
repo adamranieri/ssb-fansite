972786cfe610df913c78149f7726e179eeaaa5ff
Adam <adam.ranieri@revature.com>
2023-02-14 10:35:22 -0500

    local storage

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
 
 
 
 export function HomePage(){
 
+    const [name,setName] = useState("");
+
+    function saveToLocalStorage(){
+        localStorage.setItem("username",name);// will save the value to localstorage
+    }
+
     return <>
         <h1>The Ultimate fan website for Super smash bros!!!</h1>
         <p>This is a website dedicated to Super Smash Bros</p>
+        <input type="text" onChange={e => setName(e.target.value)} />
+        <button onClick={saveToLocalStorage}>Remember Me</button>
     
     </>
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,24 @@
 
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import {Link} from "react-router-dom"
 import { styleContext } from "../App"
 
 export function NavBar(){
     
     const {setGlobalStyle} = useContext(styleContext)
+    const [name,setName] = useState("")
+
+    useEffect(()=>{
+        setName(localStorage.getItem("username")?? "No Name Set")
+    },[]);// executes when the component loads the first time
+    // DO NOT OVERLY RELY ON LOCAL STORAGE. DO NOT USE AS A SUBSTITUE FOR STATE MANAGEMENT IN REACT
+    // using local storage to intitilize values in a redux store is a very common usecase
 
     return <>
         <Link to="/">Home</Link>
         <Link to="/fighters">Fighters</Link>
         <Link to="/arenas">Arenas</Link>
         <button onClick={()=>setGlobalStyle({color:"blue"})}>Make Blue</button>
+        <p>Welcome {name}</p>
     </>
 }
